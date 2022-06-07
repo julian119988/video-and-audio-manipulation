801620b00bc3347c7381371ebc5af830dc803ab8
julian119988 <julian.zambroni@gmail.com>
2022-06-07 17:20:52 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,6 @@ import "./App.css";
 import styled from "styled-components";
 import * as Tone from "tone";
 
-// const Blur = styled.div`
-//   position: "relative",
-//   &:before: {
-// content: " ",
-// position: "absolute",
-// background: "rgba(0, 0, 0, 0.5)",
-//   },
-// `;
-const Blur = styled.div`
-position:absolute;
-height: 394px;
-pointer-events: none;
-top: 0;
-left: 0;
-width: 700px;
-  &:after {
-    content: "";
-    filter: blur(${(props: { blur: number }) => props.blur}px);
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    pointer-events: none;
-    background-color: white;
-    opacity: .2;
-  },
-`;
 function App() {
   const [videoFile, setVideoFile] = useState<File | null>();
   const [isAudioPlaying, setIsAudioPlaying] = useState(false);
@@ -38,16 +10,21 @@ function App() {
   const [duration, setDuration] = useState(0);
   let currentTime = 0;
   let startingPitch = 10;
-  const [blurValue, setBlurValue] = useState(10);
+  const [blurValue, setBlurValue] = useState(0);
   const animationRef = useRef<any>();
   const videoTagRef = useRef<HTMLVideoElement>(null);
   const playerRef = useRef<Tone.Player | null>();
   const pitchRef = useRef<any>();
+  let counter = 0;
 
   const handleVideoInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (videoFile) {
+      playerRef.current?.disconnect();
+    }
     if (event.target.files) {
       setIsAudioLoading(true);
       setVideoFile(event.target.files[0]);
+
       var tempVideo = document.createElement("video");
       tempVideo.src = URL.createObjectURL(event.target.files[0]);
       tempVideo.preload = "metadata";
@@ -72,17 +49,18 @@ function App() {
 
   const isPlaying = () => {
     // animationRef.current = requestAnimationFrame(whilePlaying);
-    animationRef.current = window.setInterval(whilePlaying, 200);
+    // animationRef.current = window.setInterval(whilePlaying, 200);
 
     //@ts-ignore
     Tone.Transport.start();
     if (videoTagRef.current) {
       videoTagRef.current?.classList.add("unBlur");
       if ((duration * 3) / 4 <= videoTagRef.current?.currentTime) {
-        videoTagRef.current.style.transition = "";
+        videoTagRef.current.style.transition = "all 0s linear";
       } else {
+        console.log((duration * 3) / 4 - videoTagRef.current?.currentTime);
         videoTagRef.current.style.transition = `all ${
-          ((duration * 3) / 4 - videoTagRef.current?.currentTime) | duration
+          (duration * 3) / 4 - videoTagRef.current?.currentTime
         }s ease-out`;
       }
     }
@@ -90,11 +68,30 @@ function App() {
   const whilePlaying = () => {
     currentTime = videoTagRef.current ? videoTagRef.current?.currentTime : 0;
     calculatePitch();
+    if (videoTagRef.current) {
+      if (videoTagRef.current?.currentTime <= 1) {
+        videoTagRef.current.classList.remove("unBlur");
+        videoTagRef.current.style.transition = "all 0s linear";
+        videoTagRef.current.style.filter = "blur(10px) !important";
+      } else {
+        if ((duration * 3) / 4 <= videoTagRef.current?.currentTime) {
+          videoTagRef.current.style.transition = "all 0.01s linear !important";
+          videoTagRef.current.style.filter = "blur(0.01px) !important";
+        } else {
+          console.log((duration * 3) / 4 - videoTagRef.current?.currentTime);
+          videoTagRef.current.style.transition = `all ${
+            (duration * 3) / 4 - videoTagRef.current?.currentTime
+          }s ease-out`;
+          videoTagRef.current.classList.add("unBlur");
+        }
+      }
+    }
     // animationRef.current = requestAnimationFrame(whilePlaying);
   };
   const isPausing = () => {
+    currentTime = videoTagRef.current ? videoTagRef.current?.currentTime : 0;
     // cancelAnimationFrame(animationRef.current);
-    clearInterval(animationRef.current);
+    // clearInterval(animationRef.current);
     if (videoTagRef.current) {
       // var computedStyle = window.getComputedStyle(videoTagRef.current),
       //   filter = computedStyle.getPropertyValue("filter");
@@ -107,9 +104,9 @@ function App() {
           "blur(" +
           ((100 - (currentTime * 100) / threeQuartersTotal) / 10).toString() +
           "px)";
+        videoTagRef.current.style.transition = "all 0s linear";
         videoTagRef.current.style.filter = filterValue;
         videoTagRef.current.classList.remove("unBlur");
-        videoTagRef.current.style.transition = "";
       }
     }
     //@ts-ignore
@@ -121,6 +118,7 @@ function App() {
     const threeQuartersTotal = duration * (3 / 4);
     const pitchStep = startingPitch / (threeQuartersTotal / 0.2);
     const currentPitch = (currentTime / 0.2) * pitchStep;
+    Tone.Transport.seconds = currentTime;
     if (pitchRef.current && pitchRef.current.pitch >= 0) {
       if (startingPitch - currentPitch >= 0) {
         pitchRef.current.pitch = startingPitch - currentPitch;
@@ -133,7 +131,9 @@ function App() {
   };
 
   const resetAudio = () => {
-    playerRef.current?.dispose();
+    //@ts-ignore
+    Tone.Transport.stop();
+    setIsAudioPlaying(false);
     // if (videoTagRef.current) videoTagRef.current.classList.remove("unBlur");
   };
 
@@ -159,11 +159,13 @@ function App() {
                 onPlaying={isPlaying}
                 onPause={isPausing}
                 onEnded={resetAudio}
+                onTimeUpdate={whilePlaying}
                 className={`blur ${isAudioPlaying && "unBlur"}`}
+                onSeeked={isPausing}
                 muted
               />
               {/* <Blur blur={a}></Blur> */}
-              <button onClick={resetAudio}>reset</button>
+              {/* <button onClick={() => }>reset</button> */}
             </div>
             {/* <button onClick={togglePlayPause}>Play</button> */}
           </>
